abf8db22a6af8268b817b7841b47472f6038f326
ethwillz <ewwilliams6@gmail.com>
2017-10-05 22:32:19 -0500

    The simple math is done, not rendering correctly but I'll firgure it out once I'm actually done with my midterms and am not using this to procrastinate while still feeling productive help me

--- src/MandelbrotSet.java
+++ src/MandelbrotSet.java
@@ -1,38 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 //https://en.wikipedia.org/wiki/Mandelbrot_set
 
-public class MandelbrotSet extends Component{
+public class MandelbrotSet extends JFrame{
+    private static BufferedImage image;
+
     public static void main(String[] args){
-        JFrame frame = new JFrame("Mandelbrot Set");
-        frame.setSize(600, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        new MandelbrotSet().setVisible(true);
+
+    }
+
+     public MandelbrotSet(){
+
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setTitle("Mandelbrot Set");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for(int pX = 0; pX < getWidth(); pX++){
+
+            for(int pY = 0; pY < getHeight(); pY++){
+
+                double xTemp;
+                double x0 = (3.5 / getWidth()) - 2.5;
+                double y0 = (3.5 / getHeight() - 2.5);
+                double x = 0, y = 0;
+
+                int iteration = 0;
+                int maxIterations = 1000;
+
+                while(Math.pow(x, 2) + Math.pow(y, 2) < 4 && iteration < maxIterations){
+                    xTemp = Math.pow(x, 2) - Math.pow(y, 2) + x0;
+                    y = 2 * x * y + y0;
+                    x = xTemp;
+                    iteration++;
+                }
+
+                image.setRGB(pX, pY, iteration << 12);
+            }
+
+        }
+
     }
 
     @Override
     public void paint(Graphics g){
-        super.paint(g);
 
-
-        //All this wrapped in for loop that goes through each pixel
-        int x0, y0; //write equation to scale this
-        int x = 0, y = 0;
-
-        int iteration = 0;
-        int maxIterations = 1000;
-
-        while(Math.pow(x, 2) + Math.pow(y, 2) < 4 && iteration < maxIterations){
-            xTemp = Math.pow(x, 2) - Math.pow(y, 2) + x0;
-            y = 2 * x * y + y0;
-            x = xtemp;
-            iteration++;
-        }
-
-        //color = palette index of iteration
-        //plot point with color
+        g.drawImage(image, 0, 0, this);
 
     }
 }
